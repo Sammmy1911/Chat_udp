66be6a93e6172a319bc970e71b321021d644614b
Sammmy1911 <sammygal1219@gmail.com>
2025-10-05 09:30:35 -0500

    Client

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,3 +1,26 @@
+import java.net.DatagramPacket; //Es el “puerto” o canal por donde se envían y reciben los mensajes usando UDP
+import java.net.DatagramSocket; //Representa un paquete de datos UDP (contiene el mensaje, la dirección IP y el puerto destino).
+import java.net.InetAddress; //Es Basicamente la direccion de del ip que deseo
+
 public class Client {
-    
+
+     public static void main(String[] args) throws Exception {
+
+    /*Esto crea un socket UDP en un puerto aleatorio disponible.
+    *El cliente no necesita especificar un puerto, 
+    *porque sólo enviará mensajes.
+    */
+    DatagramSocket socket = new DatagramSocket();
+    String message = "Hello World"; // Mensaje Basico
+    /*Como no podemos mandar un mensaje directamenet como String
+     * Lo que hacemos es que lo convertimos en un String de Bytes
+     * Basicamente secuenciarlo
+     */
+    byte[] data = message.getBytes();
+    DatagramPacket packet = 
+    new DatagramPacket(data, data.length, 
+    InetAddress.getByName("192.168.131.42"), 5000);
+
+
+     }
 }
